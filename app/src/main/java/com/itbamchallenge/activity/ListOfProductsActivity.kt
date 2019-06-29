a254a1768306061778d2a4d2a6eab3142407a2f3
Paulo <pssfjr@gmail.com>
2019-06-29 12:04:57 -0400

    Activity and Layout Modules - Creation of classes

---
+++ app/src/main/java/com/itbamchallenge/activity/ListOfProductsActivity.kt
@@ -0,0 +1,14 @@
+package com.itbamchallenge.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.itbamchallenge.R
+
+class ListOfProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_of_products)
+    }
+
+}
