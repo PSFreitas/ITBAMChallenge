52945ce332ab7b7741824a6f5f9ed11d51d60ac9
Paulo <pssfjr@gmail.com>
2019-06-30 10:31:04 -0400

    Layout and Adapter Module - Implementation of methods

--- app/src/main/java/com/itbamchallenge/adapter/ProductListAdapter.kt
+++ app/src/main/java/com/itbamchallenge/adapter/ProductListAdapter.kt
@@ -7,13 +7,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.itbamchallenge.R
 import com.itbamchallenge.model.Product
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.product_item.view.*
 
-class ProductListAdapter(private var products : List<Product>,
-                         private var context: Context) : RecyclerView.Adapter<ProductListAdapter.ViewHolder>() {
+class ProductListAdapter(
+    private var products: List<Product>,
+    private var context: Context
+) : RecyclerView.Adapter<ProductListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.product_item,parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.product_item, parent, false)
         return ViewHolder(view)
     }
 
@@ -23,19 +26,29 @@ class ProductListAdapter(private var products : List<Product>,
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val product = products[position]
-        holder?.let{
+        holder?.let {
             it.bindView(product)
         }
     }
 
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bindView(product: Product){
+        fun bindView(product: Product) {
             val productName = itemView.product_item_name
             val productPrice = itemView.product_item_price
+            val productPicture = itemView.product_item_picture
             productName.text = product.name
             productPrice.text = product.actualPrice
+
+
+            if(product.image.isNotEmpty()){
+                Picasso
+                    .get()
+                    .load(product.image)
+                    .into(productPicture)
+            }
+
         }
 
     }
