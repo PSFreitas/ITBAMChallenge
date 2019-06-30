a2fa7b70e0f44b7ec7fea1194304f154a16c3701
Paulo <pssfjr@gmail.com>
2019-06-29 20:25:39 -0400

    Layout,Adapter and Activity Module - Creation, Refactor and Implementation of methods

--- app/src/main/java/com/itbamchallenge/activity/ListOfProductsActivity.kt
+++ app/src/main/java/com/itbamchallenge/activity/ListOfProductsActivity.kt
@@ -1,14 +1,55 @@
 package com.itbamchallenge.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.itbamchallenge.R
+import com.itbamchallenge.adapter.ProductListAdapter
+import com.itbamchallenge.model.Product
+import com.itbamchallenge.network.ProductResponse
+import com.itbamchallenge.network.ProductsService
+import kotlinx.android.synthetic.main.activity_list_of_products.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListOfProductsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_of_products)
+
+        initializeProductsList()
+
+    }
+
+    fun initializeProductsList(){
+        val call = ProductsService.create().getProducts()
+
+        call.enqueue(object : Callback<ProductResponse> {
+            override fun onResponse(call: Call<ProductResponse>, response: Response<ProductResponse>) {
+                response.body()?.let { products ->
+                    populateProductList(products.products)
+                }
+            }
+
+            override fun onFailure(call: Call<ProductResponse>, t: Throwable) {
+                Log.e("onFailure error", t?.message)
+            }
+        })
+
+    }
+
+    private fun populateProductList(products: List<Product>) {
+        findViewById<RecyclerView>(R.id.product_list_recyclerview).apply {
+           setHasFixedSize(true)
+           layoutManager = LinearLayoutManager(context)
+           adapter = ProductListAdapter(products,context)
+
+       }
     }
 
 }
---
+++ app/src/main/java/com/itbamchallenge/adapter/ProductListAdapter.kt
@@ -0,0 +1,41 @@
+package com.itbamchallenge.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.itbamchallenge.R
+import com.itbamchallenge.model.Product
+import kotlinx.android.synthetic.main.product_item.view.*
+
+class ProductListAdapter(private var products : List<Product>,
+                         private var context: Context) : RecyclerView.Adapter<ProductListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_item,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = products[position]
+        holder?.let{
+            it.bindView(product)
+        }
+    }
+
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(product: Product){
+            val productName = itemView.product_item_name
+            productName.text = product.name
+        }
+
+    }
+
+}
